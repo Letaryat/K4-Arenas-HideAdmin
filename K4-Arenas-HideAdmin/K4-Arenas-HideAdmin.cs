17090710b0462afc2e1c12f16ba2f73e527ed7b7
Letaryat <69041891+Letaryat@users.noreply.github.com>
2025-05-14 10:51:32 +0200

    0.0.1

---
+++ K4-Arenas-HideAdmin/K4-Arenas-HideAdmin.cs
@@ -0,0 +1,77 @@
+﻿using System.Text.Json.Serialization;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Capabilities;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Commands;
+using K4ArenaSharedApi;
+using Microsoft.Extensions.Logging;
+
+namespace K4_Arenas_HideAdmin;
+
+public class SampleConfig : BasePluginConfig
+{
+    [JsonPropertyName("Permission")] public string Permission { get; set; } = "@css/kick";
+
+}
+
+public class K4_Arenas_HideAdmin : BasePlugin, IPluginConfig<SampleConfig>
+{
+    public override string ModuleName => "K4 Arenas HideAdmin";
+    public override string ModuleVersion => "0.0.1";
+    public required SampleConfig Config { get; set; }
+    public static IK4ArenaSharedApi? SharedAPI_Arena { get; private set; }
+    public (bool ArenaFound, bool Checked) ArenaSupport = (false, false);
+    private List<int> hiddenAdmins = new List<int>();
+
+    public void OnConfigParsed(SampleConfig config)
+    {
+        Config = config;
+    }
+
+    public override void Load(bool hotReload)
+    {
+        Console.WriteLine("Hello World!");
+        AddCommandListener("css_hide", OnHide);
+    }
+
+    public void PerformAFKAction(CCSPlayerController player, bool afk)
+    {
+        if (!ArenaSupport.Checked)
+        {
+            string arenaPath = Path.GetFullPath(Path.Combine(ModuleDirectory, "..", "K4-Arenas"));
+            ArenaSupport.ArenaFound = Directory.Exists(arenaPath);
+            ArenaSupport.Checked = true;
+        }
+
+        if (!ArenaSupport.ArenaFound)
+        {
+            return;
+        }
+
+        if (SharedAPI_Arena is null)
+        {
+            PluginCapability<IK4ArenaSharedApi> Capability_SharedAPI = new("k4-arenas:sharedapi");
+            SharedAPI_Arena = Capability_SharedAPI.Get();
+        }
+        SharedAPI_Arena?.PerformAFKAction(player, afk);
+    }
+
+    private HookResult OnHide(CCSPlayerController? player, CommandInfo commandInfo)
+    {
+        if (player == null || player.IsBot || player.IsHLTV || player.PlayerPawn.Value == null) return HookResult.Continue;
+        var slot = player.Slot;
+        if (!AdminManager.PlayerHasPermissions(player, Config.Permission)) return HookResult.Continue;
+        if (!hiddenAdmins.Contains(slot))
+        {
+            hiddenAdmins.Add(slot);
+            PerformAFKAction(player, true);
+        }
+        else
+        {
+            hiddenAdmins.Remove(slot);
+            PerformAFKAction(player, false);
+        }
+        return HookResult.Continue;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # K4-Arenas-HideAdmin
-Listener for !hide command that sets AFK status for Admin
+Listener for **!hide** command that sets K4-Arenas player status to AFK if player have a specific permission flags. Thanks to that admins can be hidden on scoreboard without getting moved from spectators to any team while being hidden on the end of the round.
+
+## Instalation
+- Download files from releases and put them into /plugins folder,
+- Restart your server,
+- Config file should be created in configs/plugins/K4-Arenas-HideAdmin
+- Setup your access flag for !hide command that your admin system use,
+- Restart your server again
+
+## Support
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/H2H8TK0L9)
